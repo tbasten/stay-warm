45a3bac5c496d45feee74fb3f8875a291a3c901b
Tim Basten <tbasten@gmail.com>
2024-09-11 16:19:38 +0800

    light shader fixed

--- Scripts/lighting.gd
+++ Scripts/lighting.gd
@@ -1,19 +1,16 @@
 extends ColorRect
 class_name lighting
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
+func _ready():
 	show()
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta):
 	var light_positions = _get_light_positions()
 	material.set_shader_parameter("number_of_lights", light_positions.size())
 	material.set_shader_parameter("lights", light_positions)
-
+	
 func _get_light_positions():
 	return get_tree().get_nodes_in_group("light").map(
-		func(light: Node2D):
+		func(light: Node2D): 
 			return light.get_global_transform_with_canvas().origin
 	)
